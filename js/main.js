7f6a20c9a4917b4ae5871d7c3ea0a9884cd5943c
J.M. Robin <johnmichaelrobin@gmail.com>
2023-01-02 19:47:51 +0800

    Complete styling for small screen devices

---
+++ js/main.js
@@ -0,0 +1,13 @@
+import daniel from "../images/image-daniel.jpg";
+import quotationSVG from "../images/bg-pattern-quotation.svg";
+import jeannette from "../images/image-jeanette.jpg";
+import jonathan from "../images/image-jonathan.jpg";
+import kira from "../images/image-kira.jpg";
+import patrick from "../images/image-patrick.jpg";
+
+document.querySelector(".js-daniel").src = daniel;
+document.querySelector(".js-quote").src = quotationSVG;
+document.querySelector(".js-jeanette").src = jeannette;
+document.querySelector(".js-jonathan").src = jonathan;
+document.querySelector(".js-kira").src = kira;
+document.querySelector(".js-patrick").src = patrick;
